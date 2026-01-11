4043be9d0dae85f067f87307ad934e14a5ed1495
Noval Faturrahman <novalfr802@gmail.com>
2026-01-12 00:44:26 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://mimio.id",
+  devToolbar: {
+    enabled: false,
+  },
   vite: {
-    plugins: [tailwindcss()]
-  }
+    // @ts-ignore - Type mismatch between Astro's Vite and Tailwind's Vite types
+    plugins: [tailwindcss()],
+  },
+  integrations: [sitemap()],
 });
