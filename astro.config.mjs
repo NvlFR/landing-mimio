feb8872ee575e22ee6ec1cb7c9ef44f130b12de8
Noval Faturrahman <novalfr802@gmail.com>
2026-01-12 18:42:00 +0700

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mimio.id",
+  site: "https://mimio.ftrhq.my.id",
   devToolbar: {
     enabled: false,
   },
